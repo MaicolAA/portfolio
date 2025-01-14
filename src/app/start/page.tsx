05b83688a99a66262b69e1454f00c784abe36fab
maicolaa <maicolaroyave10@gmail.com>
2025-01-13 22:07:58 -0500

    first Build

---
+++ src/app/start/page.tsx
@@ -0,0 +1,18 @@
+const Start = () => {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <ol className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+                    <li className="mb-2">
+                        HI, I&#39;m Maicol Alvarez, a software engineer and a designer. I&#39;m
+                        passionate about creating beautiful and functional user interfaces.
+                        I&#39;m excited to bring my skills and creativity to the next level by
+                        building web applications with Next.js.
+                    </li>
+                </ol>
+            </main>
+        </div>
+    );
+};
+
+export default Start;
